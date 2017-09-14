71c0510fd65d2616efd74559f0e4f01f4fd74eb8
Roman Skaletskiy <roman.skaletskiy@dataart.com>
2017-09-14 17:37:58 +0300

    card create

--- js/controllers/listsCtrl.js
+++ js/controllers/listsCtrl.js
@@ -1,9 +1,8 @@
 angular.module('app').controller('listsCtrl', function(listFactory) {
-  console.log('listsCtrl');
+
   this.lists = listFactory.getList();
 
   this.addList = function() {
-    console.log(this.listName);
     listFactory.addList(this.listName);
     this.listName = '';
   }
